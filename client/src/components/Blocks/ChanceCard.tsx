53392044b325565378edd0f83387ae51f1adec53
Divyanshu Lohani <divyanshulohani2305@gmail.com>
2024-08-27 19:21:29 +0530

    Remove console log

--- client/src/components/Blocks/ChanceCard.tsx
+++ client/src/components/Blocks/ChanceCard.tsx
@@ -1,7 +1,6 @@
 import { CircleHelp } from "lucide-react";
 
 export default function ChanceCard() {
-  console.log("Hello");
   return (
     <div className="h-full w-full bg-gray-800 flex flex-col items-center justify-center rounded-lg">
       <CircleHelp />
--- client/src/components/Blocks/TresureCard.tsx
+++ client/src/components/Blocks/TresureCard.tsx
@@ -1,7 +1,6 @@
 import { Codesandbox } from "lucide-react";
 
 export default function TresureCard() {
-  console.log("Hello");
   return (
     <div className="h-full w-full bg-red-800 flex flex-col items-center justify-center rounded-lg">
       <Codesandbox />
