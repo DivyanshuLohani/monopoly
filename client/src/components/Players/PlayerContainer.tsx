e71cab74a1153f30d41ccabbc7908e7bd0712d9e
Divyanshu Lohani <divyanshulohani2305@gmail.com>
2024-09-03 16:28:20 +0530

    add player indicator

--- client/src/components/Players/PlayerContainer.tsx
+++ client/src/components/Players/PlayerContainer.tsx
@@ -15,7 +15,7 @@ const PlayerDisplay: React.FC = () => {
 
   return (
     <motion.div
-      className="flex flex-col self-start bg-gray-900 rounded-lg w-full p-3 gap-2 overflow-hidden"
+      className="flex flex-col self-start bg-gray-900 rounded-lg w-full p-3 gap-2 overflow-hidden relative"
       layout // Add the layout prop to animate layout changes smoothly
     >
       {room.players.length === 0 && (
@@ -24,39 +24,55 @@ const PlayerDisplay: React.FC = () => {
       <AnimatePresence>
         {room.players.map((player) => (
           <motion.div
-            className="p-4 rounded shadow-md bg-slate-950"
+            className="relative flex items-center"
             initial="hidden"
             animate="visible"
-            exit={{ x: "100%", opacity: 0 }} // Animation on exit
+            exit={{ x: "100%", opacity: 0 }}
             variants={slideInVariants}
             transition={{ duration: 0.2 }}
             key={player.id}
-            layout // Add the layout prop to animate the element's layout change
+            layout
           >
-            <div className="flex items-center gap-2">
-              <div
-                className="w-4 h-4 rounded-full"
-                style={{ backgroundColor: player.appearance }}
-              ></div>
-              <h2 className="text-xl font-bold">{player.name}</h2>
-              {room.host?.id === player.id && (
-                <span>
-                  <Crown />
-                </span>
+            {/* Indicator bar */}
+            {room.state !== GameState.Lobby &&
+              room.currentPlayer.id === player.id && (
+                <motion.div
+                  className="absolute left-0 w-1 h-full transition-colors duration-150"
+                  layoutId="indicator" // This ID will allow the bar to smoothly move between players
+                  initial={false} // Prevent initial animation on mount
+                  animate={{ x: 0 }}
+                  transition={{ type: "spring", stiffness: 300, damping: 30 }}
+                  style={{ backgroundColor: player.appearance }}
+                />
               )}
-            </div>
-            {room.state !== GameState.Lobby && (
-              <p className="">
-                <span className="opacity-70">$ </span>
-                <span
-                  className={`text-lg font-semibold ${
-                    player.money < 0 ? "text-red-600" : "text-white"
-                  }`}
-                >
-                  {player.money}
-                </span>
-              </p>
-            )}
+
+            {/* Player details */}
+            <motion.div className="flex-grow p-4 rounded shadow-md bg-slate-950">
+              <div className="flex items-center gap-2">
+                <div
+                  className="w-4 h-4 rounded-full"
+                  style={{ backgroundColor: player.appearance }}
+                ></div>
+                <h2 className="text-xl font-bold">{player.name}</h2>
+                {room.host?.id === player.id && (
+                  <span>
+                    <Crown />
+                  </span>
+                )}
+              </div>
+              {room.state !== GameState.Lobby && (
+                <p className="">
+                  <span className="opacity-70">$ </span>
+                  <span
+                    className={`text-lg font-semibold ${
+                      player.money < 0 ? "text-red-600" : "text-white"
+                    }`}
+                  >
+                    {player.money}
+                  </span>
+                </p>
+              )}
+            </motion.div>
           </motion.div>
         ))}
       </AnimatePresence>
